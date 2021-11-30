ae9c27993dccad6d11d9246e7002425f4e5e655f
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-11-30 16:57:30 +0000

    Add array methods

--- topic2.js
+++ topic2.js
@@ -228,3 +228,50 @@ console.log(myTrim.trim());
 console.log(myTrim.trim().length);
 
 // ##############################
+
+// Variables, Data Types and Functions: Array methods
+
+let names = ["david", "eddie", "alex", "michael"];
+let others = ["sammy", "gary", "wolfgang", "mark"];
+
+let lost = [4, 8, 15, 16, 23, 42];
+let fibonacci = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+
+let combine = lost.concat(fibonacci);
+console.log(combine);
+
+console.log(fibonacci.join("~"));
+
+// push, pop
+console.log(lost.shift());
+console.log(lost);
+
+console.log(lost.unshift(1, 2, 3));
+console.log(lost);
+
+console.log(names);
+console.log(names.reverse());
+
+console.log(names.sort());
+
+console.log(others.indexOf("gary"));
+
+console.log(combine.lastIndexOf(1));
+
+// map
+let filtered = combine.filter((x) => {
+    if (x >= 15) {
+        return x;
+    }
+});
+console.log(filtered);
+
+names.forEach((n) => console.log(n));
+
+console.log(filtered.every((num) => num < 10));
+console.log(filtered.every((num) => num > 14));
+
+console.log(fibonacci.some((num) => num > 50));
+console.log(fibonacci.some((num) => num > 100));
+
+// ##############################
