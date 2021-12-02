d74511aa3d6f933c44e2918ad701753834c7b013
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-12-02 15:22:25 +0000

    Add dates and days of the week

--- topic2-2.js
+++ topic2-2.js
@@ -22,3 +22,23 @@ new_years_date.setDate(1);
 
 let days_left = datesDiff(current_date, new_years_date);
 console.log(`${days_left} left till new year`);
+
+function getTheDay(date1) {
+    let myDate = new Date(date1);
+    let weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    let n = weekday[myDate.getDay()];
+    alert(
+        "The day of the week is " +
+        n +
+        "for the following date: " +
+        myDate.toString(),
+    );
+}
